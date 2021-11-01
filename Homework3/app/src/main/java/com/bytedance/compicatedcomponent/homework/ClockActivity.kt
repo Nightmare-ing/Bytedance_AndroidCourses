b16b0172ad0d12b5348080a0011f04e04a870a64
Qingsong Hu <smile_smiling@icloud.com>
2021-11-01 15:33:23 +0800

    upload homework3

---
+++ Homework3/app/src/main/java/com/bytedance/compicatedcomponent/homework/ClockActivity.kt
@@ -0,0 +1,32 @@
+package com.bytedance.compicatedcomponent.homework
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bytedance.compicatedcomponent.R
+import com.bytedance.compicatedcomponent.homework.ClockView.Companion.UPDATE_SECOND_POINTER
+
+/**
+ *  author : neo
+ *  time   : 2021/10/30
+ *  desc   :
+ */
+class ClockActivity : Activity() {
+
+    lateinit var clock: ClockView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clock)
+    }
+
+    val handler: Handler = Handler(Looper.getMainLooper()) { msg ->
+        when(msg.what) {
+            UPDATE_SECOND_POINTER -> {
+
+            }
+        }
+        true
+    }
+}

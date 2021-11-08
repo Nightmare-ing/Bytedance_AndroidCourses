648511bc45f9a34db7ab0f74ff64b34c32958679
Qingsong Hu <75839302+Nightmare-ing@users.noreply.github.com>
2021-11-08 14:11:03 +0800

    Established the project

---
+++ Homework4/app/src/main/java/com/dream/translate/API/Aligned_words.java
@@ -0,0 +1,21 @@
+package com.dream.translate.API;
+
+public class Aligned_words {
+
+        private Src src;
+        private Tran tran;
+        public void setSrc(Src src) {
+            this.src = src;
+        }
+        public Src getSrc() {
+            return src;
+        }
+
+        public void setTran(Tran tran) {
+            this.tran = tran;
+        }
+        public Tran getTran() {
+            return tran;
+        }
+
+}
